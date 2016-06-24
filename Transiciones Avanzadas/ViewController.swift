7ad417d4fe94a6e9de0432fc448e689ee7902dc7
Alexis Araujo <ala1289@gmail.com>
2016-06-24 10:47:51 -0500

    Código transiciones avanzadas
    app para revisar transiciones avanzadas entre dos pantallas al
    presionar el boton lleva a la segunda pantalla y al seleccionar el
    valor de la lista regresa a la pantalla con el valor seleccionado de la
    tabla.

---
+++ Transiciones Avanzadas/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Transiciones Avanzadas
+//
+//  Created by Alexis Araujo on 6/24/16.
+//  Copyright © 2016 Alexis Araujo. All rights reserved.
+//
+
+import UIKit
+
+var rowCounter = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(rowCounter)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
